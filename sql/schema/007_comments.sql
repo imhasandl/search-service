7a2564baadbf16c3d82f1edf04b4b6da7e3bbb2f
Hasan <123514871+imhasandl@users.noreply.github.com>
2025-02-25 11:27:47 +0500

    db change

--- sql/schema/007_comments.sql
+++ sql/schema/007_comments.sql
@@ -2,7 +2,7 @@
 CREATE TABLE comments (
    id UUID NOT NULL PRIMARY KEY, 
    created_at TIMESTAMP NOT NULL, 
-   post_id UUID NOT NULL REFERENCES posts(id),
+   post_id UUID NOT NULL REFERENCES posts(id) ON DELETE CASCADE,
    user_id UUID NOT NULL REFERENCES users(id),
    comment_text TEXT NOT NULL
 );
